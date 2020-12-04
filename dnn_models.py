e4ced40b9e13059e138cb758d7ed03f28519f1a9
rperi <rperi@usc.edu>
2020-12-03 18:41:47 -0800

    Added functionality to load model on device

--- dnn_models.py
+++ dnn_models.py
@@ -1,9 +1,11 @@
 from sail_dev.models import RawAudioCNN
 import torch
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 def get_model(weights_file=None):
     if weights_file is None:
         model = RawAudioCNN(num_class=40)
     else:
-        model = torch.load(weights_file)	
+        model = torch.load(weights_file, map_location=device)
     return model
